bf493532ea0c09daa4a34984ccda7189095e7264
Jayne-o <66448516+Jayne-o@users.noreply.github.com>
2025-07-06 01:31:34 -0600

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,25 @@
+SELECT 
+    B.booking_id,
+    B.booking_date,
+    U.user_id,
+    U.name
+FROM Bookings B
+INNER JOIN Users U ON B.user_id = U.user_id;
+
+SELECT 
+    P.property_id,
+    P.name AS property_name,
+    R.review_id,
+    R.rating,
+    R.comment
+FROM Properties P
+LEFT JOIN Reviews R ON P.property_id = R.property_id;
+
+SELECT 
+    U.user_id,
+    U.name,
+    B.booking_id,
+    B.booking_date,
+    B.property_id
+FROM Users U
+FULL OUTER JOIN Bookings B ON U.user_id = B.user_id;
